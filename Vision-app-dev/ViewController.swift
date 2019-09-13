6399cd5e0b33d8a6fed65e4832458a98b9b7e762
juger rash <jugerrash@jugers-Mac.local>
2019-09-13 00:01:33 -0700

    Vision-app-dev has just been created

---
+++ Vision-app-dev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vision-app-dev
+//
+//  Created by juger rash on 12.09.19.
+//  Copyright © 2019 juger rash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
